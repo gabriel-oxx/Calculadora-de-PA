e5755a3dd80e06b4f4517e255d745e36e206c11a
Gabriel <gabrielcsilvasg@gmail.com>
2021-06-18 22:44:03 -0300

    Arquivos adicionados

--- js/pa.js
+++ js/pa.js
@@ -1,4 +1,12 @@
-function calculaPa(){
-  document.write("Olá, jovem!");
+document.getElementById("cabecalio").innerHTML = "Insira os termos de sua progressão aritmética abaixo" + "<br><br>";
+function calculaPa() {
+  var primeiroTermo = parseInt(document.getElementById("primeiro-termo").value);
+  var razao = parseInt(document.getElementById("razao").value);
+  var ultimoTermo = parseInt(document.getElementById("ultimo-termo").value);
+  var valor = primeiroTermo;
+  var resultado = document.getElementById("resultado");
+  for (let i = 0; i <= ultimoTermo; i++) {
+    valor += razao;
+    resultado.innerHTML += '<p>' + valor + '</p>';
+  }
 }
-document.write(calculaPa());
